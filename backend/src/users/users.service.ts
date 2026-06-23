f918608ef904777bdcdab6b48b0e0d5796534580
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-06-23 16:18:20 -0300

    feat: integrate TypeORM for user management and refactor UsersService methods

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,59 +2,63 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdateUserDTO } from './dto/update-user.dto';
-import { randomUUID } from 'crypto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  private users: User[] = [];
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) {}
 
   throwNotFoundException(): never {
-    throw new NotFoundException('Note not found!');
+    throw new NotFoundException('User not found!');
   }
 
-  create(dto: CreateUserDTO) {
-    const note = {
-      id: randomUUID().toString(),
-      ...dto,
-      createdAt: new Date()
-    };
+  async create(dto: CreateUserDTO) {
+    try {
+      const newUser = this.userRepository.create(dto);
 
-    this.users.push(note);
+      await this.userRepository.save(newUser);
 
-    return note;
+      return newUser;
+    } catch (error) {
+      console.log(typeof error);
+
+      // if (error? === '23505') {
+      //   throw new ConflictException('Email já cadastrado');
+      // }
+
+      throw error;
+    }
   }
 
-  findAll() {
-    return this.users;
+  async findAll() {
+    const users = await this.userRepository.find({ order: { createdAt: 'desc' } });
+
+    return users;
   }
 
-  findOne(id: string) {
-    return this.users.find(note => note.id === id);
+  async findOne(id: string) {
+    const user = await this.userRepository.findOneBy({ id });
+
+    if (!user) this.throwNotFoundException();
+
+    return user;
   }
 
-  update(id: string, dto: UpdateUserDTO) {
-    const note = this.users.find(note => note.id === id);
+  async update(id: string, dto: UpdateUserDTO) {
+    const updatedUser = await this.userRepository.preload({ id, ...dto });
 
-    if (!note) this.throwNotFoundException();
+    if (!updatedUser) this.throwNotFoundException();
 
-    const newNotes = this.users.map(n => {
-      if (n.id === note.id) {
-        return { ...n, ...dto };
-      }
-
-      return n;
-    });
-
-    this.users = newNotes;
-
-    return this.users.find(note => note.id === id);
+    return await this.userRepository.save(updatedUser);
   }
 
-  remove(id: string) {
-    const note = this.users.find(note => note.id === id)!;
+  async remove(id: string) {
+    const user = await this.findOne(id);
 
-    this.users = this.users.filter(n => n.id !== note.id);
-
-    return note;
+    return await this.userRepository.remove(user);
   }
 }
