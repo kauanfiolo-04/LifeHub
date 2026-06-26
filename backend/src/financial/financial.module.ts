1435ce82b070f995a3d93d6813154776c1d5bb48
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-06-26 10:31:01 -0300

    feat: add Financial module, controller, and service

---
+++ backend/src/financial/financial.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { FinancialService } from './financial.service';
+import { FinancialController } from './financial.controller';
+
+@Module({
+  providers: [FinancialService],
+  controllers: [FinancialController]
+})
+export class FinancialModule {}
