e81d29365f3842fef68a9241d57ab9532eee0c48
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-06-26 14:18:32 -0300

    feat: implement categories management with create, update, find, and delete functionalities

--- backend/src/finance/categories/categories.controller.ts
+++ backend/src/finance/categories/categories.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { TokenPayload } from '../../auth/decorators/user.decorator';
+import { type JwtPayload } from '../../auth/types/jwt-payload.type';
+import { CreateCategoryDTO } from './dto/create-category.dto';
+import { UpdateCategoryDTO } from './dto/update-category.dto';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  create(@Body() body: CreateCategoryDTO, @TokenPayload() payload: JwtPayload) {
+    return this.categoriesService.createAccount(body, payload);
+  }
+
+  @Get()
+  findAll() {
+    return this.categoriesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.categoriesService.findOne(id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: UpdateCategoryDTO, @TokenPayload() payload: JwtPayload) {
+    return this.categoriesService.update(id, body, payload);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  remove(@Param('id') id: string, @TokenPayload() payload: JwtPayload) {
+    return this.categoriesService.remove(id, payload);
+  }
+}
--- backend/src/finance/categories/categories.service.ts
+++ backend/src/finance/categories/categories.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Repository } from 'typeorm';
+import { CreateCategoryDTO } from './dto/create-category.dto';
+import { JwtPayload } from '../../auth/types/jwt-payload.type';
+import { UpdateCategoryDTO } from './dto/update-category.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoriesRepository: Repository<Category>
+  ) {}
+
+  throwNotFoundException(): never {
+    throw new NotFoundException('Category not found!');
+  }
+
+  async createAccount(dto: CreateCategoryDTO, payload: JwtPayload) {
+    const newAccount = this.categoriesRepository.create({
+      ...dto,
+      user: { id: payload.sub }
+    });
+
+    await this.categoriesRepository.save(newAccount);
+  }
+
+  async findAll() {
+    const tasks = await this.categoriesRepository.find({ order: { createdAt: 'desc' } });
+
+    return tasks;
+  }
+
+  async findOne(id: string) {
+    const account = await this.categoriesRepository.findOneBy({ id });
+
+    if (!account) this.throwNotFoundException();
+
+    return account;
+  }
+
+  async update(id: string, dto: UpdateCategoryDTO, payload: JwtPayload) {
+    const updatedAccount = await this.categoriesRepository.preload({ id, ...dto });
+
+    if (!updatedAccount) this.throwNotFoundException();
+
+    if (payload.sub !== updatedAccount.user.id)
+      throw new UnauthorizedException(`You can't change another user category.`);
+
+    return await this.categoriesRepository.save(updatedAccount);
+  }
+
+  async remove(id: string, payload: JwtPayload) {
+    const category = await this.findOne(id);
+
+    if (payload.sub !== category.user.id) throw new UnauthorizedException(`You can't delete another user category.`);
+
+    return await this.categoriesRepository.remove(category);
+  }
+}
