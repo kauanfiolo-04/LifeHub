0145bcb38294906d9f3b2e31447a70581bc34094
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-06-30 14:46:13 -0300

    feat: update metadata and enhance homepage layout for LifeHub

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <div className="min-h-screen bg-background text-foreground flex items-center justify-center">
-      LifeHub OK 🚀
-    </div>
+    <main className="h-screen flex flex-col items-center justify-center gap-40">
+      <h1 className="text-4xl font-bold">LifeHub</h1>
+
+      <div className="flex flex-col items-center justify-center">
+        <p>
+          Organize your ideas, tasks and finance in just one place!
+        </p>
+
+        <p>If you like the idea you should register an account and start using</p>
+      </div>
+    </main>
   );
 }
