15ddf28f4943bf59a0dc12302485850ca9273454
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-07-02 19:39:21 -0300

    feat: enable CORS and add global exception filter in app configuration

--- backend/src/app/config/app.config.ts
+++ backend/src/app/config/app.config.ts
@@ -1,6 +1,9 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { TypeOrmExceptionFilter } from '../../common/filters/typeorm/typeorm.filter';
 
 export default (app: INestApplication) => {
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // remove os campos que nao estao no DTO
@@ -8,6 +11,8 @@ export default (app: INestApplication) => {
       transform: false // tenta transformar os types dos dados de param e dtos, custa performance
     })
   );
+
+  app.useGlobalFilters(new TypeOrmExceptionFilter());
 
   return app;
 };
