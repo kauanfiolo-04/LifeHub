42dbcf016f18095f6b7d760627f1fe521c91ae27
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-07-03 14:41:48 -0300

    feat: enhance login and logout functionality with cookie management for refresh tokens

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Post, Req, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { SignupDTO } from './dto/signup.dto';
 import { LoginDto } from './dto/login.dto';
@@ -8,10 +9,14 @@ import { type JwtPayload } from './types/jwt-payload.type';
 import { GithubAuthGuard } from './guards/github-auth.guard';
 import { GoogleAuthGuard } from './guards/google-auth.guard';
 import { type RequestWithOAuth } from './types/oauth-profile.type';
+import { type Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService
+  ) {}
 
   @Post('signup')
   signup(@Body() body: SignupDTO) {
@@ -19,16 +24,32 @@ export class AuthController {
   }
 
   @Post('login')
-  async login(@Body() dto: LoginDto) {
+  async login(@Body() dto: LoginDto, @Res({ passthrough: true }) res: Response) {
     const user = await this.authService.validateUser(dto);
 
-    return this.authService.login(user);
+    const { accessToken, refreshToken } = await this.authService.login(user);
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: this.configService.get<string>('globalConfig.environment') == 'production',
+      maxAge: this.configService.get<number>('globalConfig.jwt.jwt_refresh_ttl')
+    });
+
+    return { accessToken };
   }
 
   @UseGuards(JwtAuthGuard)
   @Post('refresh')
   refresh(@Body() body: { refreshToken: string }, @TokenPayload() payload: JwtPayload) {
     return this.authService.refreshTokens(payload.sub, body.refreshToken);
+  }
+
+  @Post('logout')
+  logout(@Res({ passthrough: true }) res: Response) {
+    res.clearCookie('refreshToken');
+
+    return { success: true };
   }
 
   @Get('google')
