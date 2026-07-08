91fb637252164a983c18f91962a8ed5d2860bdde
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-07-07 23:45:03 -0300

    feat: implement OAuth button click handling for GitHub and Google authentication

--- frontend/src/components/auth/oauth-buttons.tsx
+++ frontend/src/components/auth/oauth-buttons.tsx
@@ -1,21 +1,28 @@
+"use client";
+
 import { HugeiconsIcon } from "@hugeicons/react";
 import { Button } from "../ui/button";
 import { Github01Icon, GoogleIcon } from "@hugeicons/core-free-icons";
 
 export default function OAuthButtons() {
-  return (
-    <div className="flex flex-row gap-2 max-w-full">
-      <Button className="flex flex-1 gap-2" size="lg">
-        <HugeiconsIcon icon={Github01Icon} width={32} />
+  const handleSubmit = (type: "github" | "google") => {
+    window.location.href =
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/${type}`;
+  }
 
-        <span>GitHub</span>
-      </Button>
+return (
+  <div className="flex flex-row gap-2 max-w-full">
+    <Button className="flex flex-1 gap-2" size="lg" onClick={() => handleSubmit("github")}>
+      <HugeiconsIcon icon={Github01Icon} width={32} />
 
-      <Button className="flex flex-1 gap-2" size="lg">
-        <HugeiconsIcon icon={GoogleIcon} width={32} />
+      <span>GitHub</span>
+    </Button>
 
-        <span>Google</span>
-      </Button>
-    </div>
-  );
+    <Button className="flex flex-1 gap-2" size="lg" onClick={() => handleSubmit("google")}>
+      <HugeiconsIcon icon={GoogleIcon} width={32} />
+
+      <span>Google</span>
+    </Button>
+  </div>
+);
 }
